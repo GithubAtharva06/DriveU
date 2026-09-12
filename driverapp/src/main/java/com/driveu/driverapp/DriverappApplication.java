59ff3044f247edbb67988279ea377ec3ee189a89
GithubAtharva06 <atharvachavan206@gmail.com>
2026-09-13 00:01:28 +0530

    first commit

---
+++ driverapp/src/main/java/com/driveu/driverapp/DriverappApplication.java
@@ -0,0 +1,13 @@
+package com.driveu.driverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverappApplication.class, args);
+	}
+
+}
