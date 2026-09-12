9edf5a875a0f7d57b3ae5e019f4b8bdbe29622aa
GithubAtharva06 <atharvachavan206@gmail.com>
2026-09-13 01:20:00 +0530

    initialized entities for the app

---
+++ driverapp/src/main/java/com/driveu/driverapp/entities/Driver.java
@@ -0,0 +1,51 @@
+package com.driveu.driverapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Driver {
+    @Id
+    private UUID id;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^(?:\\+91|91)?[6-9]\\d{9}$",
+            message = "Invalid Indian phone number. Must be a valid 10-digit number optionally starting with +91 or 91."
+    )
+    private String phoneNo;
+
+    @NotBlank
+    private String userName;
+    @Email
+    @NotBlank
+    private String email;
+
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*]).{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (@#$%^&+=!*)"
+    )
+    private String password;
+
+    @NotBlank
+    private String licenseNumber;
+    @Enumerated(EnumType.STRING)
+    private StatusCheck status;
+    private LocalDateTime createdAt;
+}
---
+++ driverapp/src/main/java/com/driveu/driverapp/entities/StatusCheck.java
@@ -0,0 +1,7 @@
+package com.driveu.driverapp.entities;
+
+public enum StatusCheck {
+    ONLINE,
+    OFFLINE,
+    SLEEP
+}
